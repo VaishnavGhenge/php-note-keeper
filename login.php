dd8aa5c0ae369ea40abec84889c836dccedb79bc
Vaishnav Ghenge <62459983+VaishnavGhenge@users.noreply.github.com>
2021-01-11 19:12:11 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'check_auth.php';
+
+if(isset($_SESSION['user']))
+{
+    header("location: home.php");
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Login</title>
+</head>
+
+<body>
+<div class="justify-content-center">
+  <div class="form">
+    <form class="login-form" action="check_auth.php" method="POST">
+      <input type="text" placeholder="Email" name="email" required />
+      <input type="password" placeholder="Password" name="pass" required />
+      <p class="message" style="text-align: right; margin-top: 0px; margin-bottom: 15px;"><a href="get_otp.php?f=1">Forgot password</a></p>
+      <button type="submit">login</button>
+      <p class="message">Not registered? <a id="lnk" href="get_otp.php">Create an account</a></p>
+    </form>
+  </div>
+</div>
+</body>
+</html>
+
+
